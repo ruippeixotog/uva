7e026c277110691a52eea164ee1aca611daf7324
Rui Gonçalves <ruippeixotog@gmail.com>
2013-10-30 22:47:13 +0000

    Solve problem 10651.

--- README.md
+++ README.md
@@ -128,6 +128,7 @@ The following is the list of the problems solved. Each problem id is specified b
 
 #### Volume 106
 
+* ✓ [Pebble Solitaire][10651] (`10651`)
 * ✓ [Work Reduction][10670] (`10670`)
 
 #### Volume 108
@@ -244,6 +245,7 @@ The following is the list of the problems solved. Each problem id is specified b
 [10363]: http://uva.onlinejudge.org/external/103/10363.html
 [10364]: http://uva.onlinejudge.org/external/103/10364.html
 [10420]: http://uva.onlinejudge.org/external/104/10420.html
+[10651]: http://uva.onlinejudge.org/external/106/10651.html
 [10670]: http://uva.onlinejudge.org/external/106/10670.html
 [10812]: http://uva.onlinejudge.org/external/108/10812.html
 [10827]: http://uva.onlinejudge.org/external/108/10827.html
---
+++ src/10651.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+
+#define P 12
+
+using namespace std;
+
+bool state[P];
+
+int findBest(int pieces) {
+  int best = pieces;
+  for(int i = 0; i < P - 1; i++) {
+    if(state[i] && state[i + 1]) {
+      state[i] = state[i + 1] = false;
+      if(i > 0 && !state[i - 1]) {
+        state[i - 1] = true;
+        best = min(best, findBest(pieces - 1));
+        state[i - 1] = false;
+      }
+      if(i + 2 < P && !state[i + 2]) {
+        state[i + 2] = true;
+        best = min(best, findBest(pieces - 1));
+        state[i + 2] = false;
+      }
+      state[i] = state[i + 1] = true;
+    }
+  }
+  return best;
+}
+
+int main() {
+  int n; cin >> n;
+
+  while(n--) {
+    string game; cin >> game;
+
+    int pieces = 0;
+    for(int i = 0; i < P; i++) {
+      state[i] = game[i] == 'o';
+      if(state[i]) pieces++;
+    }
+    cout << findBest(pieces) << endl;
+  }
+  return 0;
+}
